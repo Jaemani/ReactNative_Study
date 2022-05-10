345890ccb2b462650a56b6204d9f3398ab0e5ecd
Jaemani <leejaeman0227@gmail.com>
2022-05-10 17:54:40 +0900

    self made version

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,81 @@
-import './app.css';
-import Habits from './components/habits';
+import "./app.css";
+import Habits from "./components/habits";
+import React, { Component } from "react";
 
-function App() {
-  return <Habits></Habits>;
+export default class App extends Component {
+    state = {
+        firstClick: true,
+        inputText: "Habit",
+        habits: [
+            { id: 1, name: "Reading", count: 0 },
+            { id: 2, name: "Running", count: 0 },
+            { id: 3, name: "Coding", count: 0 },
+        ],
+    };
+
+    addHabit = () => {
+        let habits = [...this.state.habits];
+        const habit = { 
+            id: habits.length+1,
+            name: this.state.inputText,
+            count: 0
+        }
+        habits.push(habit);
+        this.state.firstClick=true;
+        this.state.inputText="Habit";
+        this.setState({habits});
+    };
+
+    inputControl = (event) => {
+        this.setState({inputText: event.target.value})
+    };
+    
+    firstClick = () => {
+        let firstClick = this.state.firstClick
+        let inputText = this.state.inputText
+        firstClick===true ? inputText="" : inputText=inputText;
+        firstClick = false;
+        this.setState({firstClick, inputText});
+    };
+
+    resetAll = () => {
+        const habits = [];
+        this.setState({habits});
+    };
+
+    render() {
+        return (
+            <nav className="app">
+                <span className="title">Habit Tracker {this.state.habits.length}</span>
+                <input 
+                    type="text"
+                    className="input"
+                    value={this.state.inputText}
+                    onClick={this.firstClick}
+                    onChange={this.inputControl}
+                >
+                </input>
+                <input 
+                    type="submit"
+                    className="submit"
+                    value="Add"
+                    onClick={this.addHabit}
+                >
+                </input>
+                <Habits 
+                    habits={this.state.habits} 
+                    appT={this}
+                />
+                <button className="reset" onClick={this.resetAll}>Reset All</button>
+            </nav>
+        );
+    }
 }
 
-export default App;
+// 컴포넌트가 아닌 function으로 정의된 App
+
+// function App() {
+//   return <Habits></Habits>;
+// }
+
+// export default App;
